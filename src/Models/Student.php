1408e8334eac649a2494b18d7a272fd8efa7a8f3
Gabriel Lorenzo S. Javierto <watdapuck23@gmail.com>
2024-10-22 22:27:34 +0800

    re-arrange code

--- src/Models/Student.php
+++ src/Models/Student.php
@@ -8,23 +8,51 @@ use \PDO;
 class Student extends BaseModel
 {
     
-    public function all()
-    {
-        $sql = "SELECT * FROM students";
-        $statement = $this->db->prepare($sql);
-        $statement->execute();
-        $result = $statement->fetchAll(PDO::FETCH_CLASS, '\App\Models\Student');
-        return $result;
-    }
+public function all()
+{
+    $sql = "SELECT * FROM students";
+    $statement = $this->db->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_CLASS, '\App\Models\Student');
+    return $result;
+}
 
-    
-    public function find($id)
-    {
-        $sql = "SELECT * FROM students WHERE id = :id";
-        $statement = $this->db->prepare($sql);
-        $statement->bindValue(':id', $id, PDO::PARAM_INT);
-        $statement->execute();
-        $result = $statement->fetchObject('\App\Models\Student');
-        return $result ? $result : null; // Return the student object or null if not found
-    }
+public function find($id)
+{
+    $sql = "SELECT * FROM students WHERE id = :id";
+    $statement = $this->db->prepare($sql);
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
+    $result = $statement->fetchObject('\App\Models\Student');
+    return $result ? $result : null; 
+}
+
+public function getStudentCode()
+{
+    $sql = "SELECT student_code FROM students";
+    $statement = $this->db->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_COLUMN); 
+    return $result;
+}
+
+public function getEmail()
+{
+    $sql = "SELECT email FROM students";
+    $statement = $this->db->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_COLUMN); 
+    return $result;
+}
+
+public function getFullName()
+{
+    $sql = "SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM students";
+    $statement = $this->db->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_COLUMN); 
+    return $result;
+}
+
+  
 }
